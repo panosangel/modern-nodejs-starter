d46d350e94451b2f9895f7f66afa1a2d9e963b25
Panos Angel <panosangel@users.noreply.github.com>
2025-03-26 19:30:01 +0200

    ESLint config / Ignore dist folder

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,9 @@ import tseslint from "typescript-eslint";
 
 
 export default defineConfig([
+  {
+    ignores: ["dist/", "tsconfig.json"] // acts as global ignores, due to the absence of other properties
+  },
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.node } },
   { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
